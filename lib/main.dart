46604162e632b5e1505864f5fce5e4987d87afbc
Muhammad Usama <imusamairshad@gmail.com>
2023-05-07 12:39:13 +0500

    modified main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,10 @@ import 'package:dice_roller_app/gradient_container.dart';
 void main() {
   runApp(const MaterialApp(
     home: Scaffold(
-      body: GradientContainer(), // This will call a constructor function.
+      body: GradientContainer(Colors.red, Colors.green),
+      // GradientContainer(
+      //   colors: [Colors.red, Colors.green],
+      // ), // This will call a constructor function.
     ),
   ));
 }
