61edb944e2955acfb979d63fa6714c88d18a456c
Muhammad Usama <imusamairshad@gmail.com>
2023-05-07 16:46:34 +0500

    modified gradient_container.dart

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:dice_roller_app/styled_text.dart';
+import 'package:dice_roller_app/dice_roller.dart';
 
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer(this.color1, this.color2, {super.key});
+
+  const GradientContainer.purple({super.key})
+      : color1 = Colors.deepPurple,
+        color2 = Colors.indigo;
 
   final Color color1;
   final Color color2;
@@ -24,7 +28,7 @@ class GradientContainer extends StatelessWidget {
         ),
       ),
       child: const Center(
-        child: StyledText("Hello World!"),
+        child: DiceRoller(), // DiceRoller's build() method is executed.
       ),
     );
   }
