be36b7c30135ebd9c3407d7cd5f75502fb56724c
Muhammad Usama <imusamairshad@gmail.com>
2023-05-07 16:45:51 +0500

    modified main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,14 @@ import 'package:dice_roller_app/gradient_container.dart';
 
 void main() {
   runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: Scaffold(
-      body: GradientContainer(Colors.red, Colors.green),
+      body:
+          // GradientContainer.purple(),
+          GradientContainer(
+        Color.fromARGB(255, 33, 5, 109),
+        Color.fromARGB(255, 68, 21, 149),
+      ),
       // GradientContainer(
       //   colors: [Colors.red, Colors.green],
       // ), // This will call a constructor function.
